e8fdba89268d956bb04b8c0bd42f880d028f7578
volkanili <volkanili@yahoo.com>
2023-04-19 14:45:43 +0300

    Initial commit: Create simple personal website with blog

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    return render_template('post.html', post_id=post_id)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
